60102f27bc4a314c5cebdd02b8e6afc0ff24f16e
Mimir <mimir@xmppwocky.net>
2021-11-15 09:28:12 -0800

    disable TLP for a try

--- machines/mimir-nixos-fw/configuration.nix
+++ machines/mimir-nixos-fw/configuration.nix
@@ -70,7 +70,7 @@
   ];
 
   services.tlp = {
-    enable = true;
+    enable = false;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
