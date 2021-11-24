c5e7c5bb6f90ea95e17c2604248c298a309eb843
Mimir <mimir@xmppwocky.net>
2021-11-23 21:41:10 -0800

    ps5 tweakery

--- modules/ps5-controller-udev.nix
+++ modules/ps5-controller-udev.nix
@@ -2,9 +2,7 @@
   boot.kernelModules = [ "hid_playstation" ];
 
   services.udev.extraRules = ''
-KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
-KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
-KERNEL=="hid_playstation*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
-KERNEL=="hid_playstation*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
+    # PS5 DualSense controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
   '';
 }
