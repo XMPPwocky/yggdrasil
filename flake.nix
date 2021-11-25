41c21588bf0aa0cd5ccfe37cf33a8d90eb862344
Mimir <mimir@xmppwocky.net>
2021-11-25 11:55:09 -0800

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     binaryninja-nix.url = "github:XMPPwocky/binaryninja-nix";
   };
