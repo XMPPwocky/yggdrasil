a6aae1ecd8f8f1177517c76d4b3d3bccbd1a5db6
Mimir <mimir@xmppwocky.net>
2022-04-04 16:21:35 -0700

    x

--- flake.nix
+++ flake.nix
@@ -1,16 +1,12 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
     let
-      customPackages = {
-        binaryninja = binaryninja-nix;
-      };
-      nixosModules = (import ./nixos/modules.nix) { inherit customPackages flake-utils home-manager; };
-      nixosConfigurations = (import ./nixos/configurations.nix) { inherit customPackages flake-utils home-manager nixosModules nixpkgs; };
+      nixosModules = (import ./nixos/modules.nix) { inherit nixpkgs; };
+      nixosConfigurations = (import ./nixos/configurations.nix) { inherit nixpkgs; };
     in
     {
       inherit nixosConfigurations nixosModules;
     };
-  };
 }
